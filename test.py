aa13d69bd16c87c3f234feb21dd3ca57bd2e81c5
Hlsky <mxt1990@live.cn>
2016-02-14 22:20:57 +0800

    complete db module. import transwarp.db to use.

--- test.py
+++ test.py
@@ -1,23 +1,85 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'description: test file'
 
 __author__ = 'hlsky'
 
 
+'''
+description: test file
+'''
 
+
+import time
+import uuid
+import logging
+import threading
+import sys
+import traceback
+
+#test Dict
+class Dict(dict):
+    '''
+    Simple dict but support access as x.y style.
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    >>> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
+    >>> d3.a
+    1
+    >>> d3.b
+    2
+    >>> d3.c
+    3
+    '''
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s  [%(levelname)s] [%(filename)s] [line:%(lineno)d]  %(message)s',
+                    datefmt='%d.%b.%Y %H:%M:%S',
+                    # filename='myapp.log',
+                    filemode='w')
+
+#test mysql 连接
+'''
 # 导入MySQL驱动:
 import mysql.connector
-'''
 # 注意把password设为你的root口令:
 conn = mysql.connector.connect(user='root', password='password', database='test', use_unicode=True)
 cursor = conn.cursor()
 # 创建user表:
 #cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
 # 插入一行记录，注意MySQL的占位符是%s:
-#cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'Michael'])
-print cursor.rowcount
+#cursor.execute('insert into user (id, name) values (%s, %s)', ['2', 'Tom'])
+#print cursor.rowcount
 # 提交事务:
 #conn.commit()
 #cursor.close()
@@ -25,19 +87,171 @@ print cursor.rowcount
 #cursor = conn.cursor()
 cursor.execute('select * from user where id = %s', ['1'])
 values = cursor.fetchall()
-print  values
+names = [x[0] for x in cursor.description]
+names = []
+print names
+print [Dict(names,x) for x in values]
+print values
 # 关闭Cursor和Connection:
 cursor.close()
 
 conn.close()
+'''
 
+#test git
+'''
 print "test git in pycharm"
 print "test dev branch"
 '''
 
-class _LazyConn(object):
-    def __init__(self):
-        mysql.connector.connect(user='root', password='password', database='test', use_unicode=True)
-        return None
+#test some dict func
+'''
+kw = {}
+params = dict(user='user', password='password', database='database', host='host', port='port')
+defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci', autocommit=False)
+for k, v in defaults.iteritems():
+    #print k,v
+    params[k] = kw.pop(k, v)
 
-print _LazyConn()
+print params
+'''
+
+#test uuid,可以用于生成唯一ID
+'''
+def next_id(t=None):
+    if t is None:
+        t = time.time()
+    return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
+
+print next_id()
+'''
+
+#test logging
+
+#print logging.debug("test")
+#print logging.info("test")
+'''
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s  [%(levelname)s] %(filename)s[line:%(lineno)d]  %(message)s',
+                datefmt='%d.%b.%Y %H:%M:%S',
+                #filename='myapp.log',
+                filemode='w')
+
+logging.debug('This is debug message')
+logging.info('This is info message')
+logging.warning('This is warning message')
+logging.warning("test")
+'''
+
+#test lambda
+'''
+def a():
+    a = 1
+    print 'test'
+
+print a
+#a()
+b = lambda:a()
+print b
+#b()
+'''
+
+
+#test thread.local
+'''
+class a(object):
+    def __init__(self):
+        pass
+class ThreadTest(threading.local):
+    def __init__(self):
+        self.count = 0
+
+    def count_plus(self):
+        #self.count = a()
+        self.count = self.count + 1
+        print self.count
+        time.sleep(10)
+
+thd_test = ThreadTest()
+
+def test_thd():
+    global thd_test
+    #thd_test = ThreadTest()
+    thd_test.count_plus()
+
+for i in range(10):
+    t1 = threading.Thread(target=test_thd)
+    t1.start()
+    #t1.join()
+    #time.sleep(2)
+'''
+
+#test dict.values
+'''
+#第一元素是固定的？
+d = { u'test':'djklsj',u'name':u'M', u'id':u'2',u'value': '1'}
+d = Dict(test='djk', name = 'M', id = '2', value = '1')
+print d
+print d.values()
+'''
+
+#test 父类、子类
+'''
+class parent(object):
+    def __init__(self):
+        print "this is parent"
+
+class son(parent):
+    def __init__(self):
+        super(son, self).__init__()
+        print "this is son"
+
+s = son()
+'''
+
+
+
+
+#test with and exception print
+'''
+def exception_logging(etype, value, tb):
+    if tb:
+        err_info = ''.join(['Traceback: ']+ traceback.format_tb(tb))
+        err_info = err_info.replace('\n', ' ')
+        logging.info(err_info)
+    if etype or value:
+        err_type = ''.join(traceback.format_exception_only(etype, value))
+        logging.error(err_type)
+
+class withtest(object):
+    def __enter__(self):
+        print "with begin"
+
+    def __exit__(self, exctype, excvalue, tb):
+        #print exctype.__name__
+        #print excvalue
+        #print dir(tb)
+        #traceback.print_tb(tb)
+        #traceback.print_exc()
+        #err = ''.join(format_exception(exctype, excvalue, tb))
+        #err = err.replace('\n', ' ')
+        exception_logging(exctype, excvalue, tb)
+        #ogging.error(err)
+        print "with end"
+
+#print sys.exc_info()
+with withtest():
+    print  [i for i in range(10)]
+    [x/0 for x in range(1,200,200)]
+try:
+    with withtest():
+        print  [i for i in range(10)]
+        [x/0 for x in range(1,200,200)]
+except:
+    pass
+'''
+
+#test transwarp.db
+from www.transwarp import db
+db.create_engine('root', 'password', 'test')
+print db.select("select * from user")
--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,69 +1,244 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""description: encapsulation sql to achieve function which can connect and control mysql"""
+__author__ = 'hlsky'
+
+'''
+description: database operation module
+'''
 
 import threading
 import mysql.connector
-
-__author__ = 'hlsky'
+import logging
+import time
+import functools
+import traceback
 
 # db.py
 
-DB_USER = 'root'
-DB_PASSWORD = 'password'
-DB_NAME = 'test'
+# logging config
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s  [%(levelname)s] [%(filename)s] [line:%(lineno)d]  %(message)s',
+                    datefmt='%d.%b.%Y %H:%M:%S',
+                    # filename='myapp.log',
+                    filemode='w')
 
-#数据库底层连接封装
-class _LasyConnection(object):
-    def __init__(self):
-        self.connect = mysql.connector.connect(user=DB_USER, password=DB_PASSWORD, database=DB_NAME, use_unicode=True)
-        self.cursormysql = self.connect.cursor()
 
-    def cursor(self):
-        return self.cursormysql
+class Dict(dict):
+    """
+    Simple dict but support access as x.y style.
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    >>> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
+    >>> d3.a
+    1
+    >>> d3.b
+    2
+    >>> d3.c
+    3
+    """
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
 
-    def connect(self):
-        return self.connect
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
 
-    def cleanup(self):
-        self.cursormysql.close()
-        self.connect.close()
+    def __setattr__(self, key, value):
+        self[key] = value
 
-# 数据库引擎对象:
+
+# profile func
+def _profiling(start, sql=''):
+    """
+    print sql execute time
+    :param start: start time
+    :param sql: sql which is executed
+    :return: none
+    """
+    t = time.time() - start
+    if t > 0.1:
+        logging.warning("[PROFILING] [DB] %s: %s" % (t, sql))
+    else:
+        logging.info("[PROFILING] [DB] %s: %s" % (t, sql))
+
+
+def exception_logging(etype, value, tb, func_name=''):
+    """
+    用于__exit__中向logging输出traceback和错误信息
+    :param etype: 错误类型
+    :param value: 错误说明
+    :param tb: traceback信息
+    :param func_name: 错误打印函数名
+    :return: None
+    """
+    if tb:
+        err_info = ''.join(['Traceback: '] + traceback.format_tb(tb))
+        err_info = err_info.replace('\n', ' ')
+        logging.info(err_info)
+    if etype or value:
+        func_str = 'in func <' + func_name + '>: ' if func_name else ''
+        err_type = func_str + ''.join(traceback.format_exception_only(etype, value))
+        # print "funcstr: ", func_str
+        err_type = err_type.replace('\n', '')
+        # print err_type
+        logging.error(err_type)
+
+
+# raise error object(错误定义对象)
+class DBError(Exception):
+    pass
+
+
+class MultiColumnsError(DBError):
+    pass
+
+
+# database engine object(数据库引擎对象)
 class _Engine(object):
     def __init__(self, connect):
+        """
+        :param connect: mysql db connect
+        :return: none
+        """
         self._connect = connect
 
     def connect(self):
+        """
+        :return: pointer of mysql connect
+        """
         return self._connect()
 
+# global engine object:
 engine = None
 
 
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+    """
+    创建engine连接
+    :param user: database username
+    :param password: database password
+    :param database: database name
+    :param host: host
+    :param port: port
+    :param kw: 其他参数
+    :return: None
+    """
+    global engine
+    if engine is not None:
+        raise DBError("Engine is already initialized.")
+    params = dict(user=user, password=password, database=database, host=host, port=port)
+    defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci', autocommit=False)
+    for k, v in defaults.iteritems():
+        params[k] = kw.pop(k, v)
+    params.update(kw)
+    # 使用lambda可以使mysql.connector.connect(**params)整体变为一个回调函数，只有在实际调用时才会运行函数。
+    engine = _Engine(lambda: mysql.connector.connect(**params))
+    # buffered参数：cursor是否立即返回fetch结果（缓存区）
+    params['buffered'] = True
+    # test connection...
+    logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
+
+
+# 数据库底层连接封装
+class _LasyConnection(object):
+    """
+    惰性连接封装，仅当需要调用cursor时才连接数据库
+    """
+    def __init__(self):
+        self.connection = None
+
+    def cursor(self):
+        """
+        只有当需要调用cursor时才会连接数据库
+        :return: None
+        """
+        if self.connection is None:
+            conn = engine.connect()
+            # logging.info('open connection <%s>...' % hex(id(connection)))
+            logging.info('[CONNECTION] [OPEN] connection <%s>...' % hex(id(connection)))
+            self.connection = conn
+        return self.connection.cursor()
+
+    def commit(self):
+        return self.connection.commit()
+
+    def rollback(self):
+        return self.connection.rollback()
+
+    def cleanup(self):
+        """
+        关闭数据库连接
+        :return: None
+        """
+        if self.connection:
+            conn = self.connection
+            self.connection = None
+            # logging.info('close connection <%s>...' % hex(id(connection)))
+            logging.info('[CONNECTION] [CLOSE] connection <%s>...' % hex(id(connection)))
+            conn.close()
+
+
 # 持有数据库连接的上下文对象:
+# threading.local的继承，会对每一个线程生成新的局部变量，即使_db_ctx是全局的
 class _DbCtx(threading.local):
     def __init__(self):
+        super(_DbCtx, self).__init__()
         self.connection = None
         self.transactions = 0
 
     def is_init(self):
+        """
+        判断数据库是否初始化（建立连接）
+        :return:
+        """
         return self.connection is not None
 
     def init(self):
+        """
+        建立数据库连接
+        :return:
+        """
+        logging.info("open lazy connection ...")
         self.connection = _LasyConnection()
         self.transactions = 0
 
     def cleanup(self):
+        """
+        关闭数据库连接
+        :return:
+        """
         self.connection.cleanup()
         self.connection = None
 
     def cursor(self):
         return self.connection.cursor()
 
+
 _db_ctx = _DbCtx()
 
 
+# 用于with方法，建立数据库连接
 class _ConnectionCtx(object):
     def __enter__(self):
         global _db_ctx
@@ -73,31 +248,136 @@ class _ConnectionCtx(object):
             self.should_cleanup = True
         return self
 
-    def __exit__(self, exctype, excvalue, traceback):
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        exception_logging(exc_type, exc_value, exc_tb, "_ConnectionCtx.__exit__")
         global _db_ctx
         if self.should_cleanup:
             _db_ctx.cleanup()
 
+
+# 当外部调用多条语句，使用with connection()的用法时，只打开一条数据库连接，和__enter__中的if判断有关
 def connection():
     return _ConnectionCtx()
 
+
 def with_connection(func):
+    """
+    装饰器
+    :param func:
+    :return:
+    """
+    @functools.wraps(func)
     def wrapper(*args, **kw):
         with connection():
             return func(*args, **kw)
     return wrapper
 
+
 @with_connection
-def select(sql, *args):
+def _select(sql, first, *args):
+    """
+    select实现函数
+    :param sql:
+    :param first:
+    :param args:
+    :return:
+    """
     global _db_ctx
-    _db_ctx.cursor().execute(sql, *args)
-    #_db_ctx.cursor().execute("select * from user where id = ?", ('1'))
-    return _db_ctx.cursor().fetchall()
+    cursor = None
+    names = []
+    sql = sql.replace('?', '%s')
+    logging.info("Sql: %s, Args: %s" % (sql, args))
+    try:
+        cursor = _db_ctx.cursor()
+        cursor.execute(sql, args)
+        if cursor.description:
+            names = [x[0] for x in cursor.description]
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names, values)
+        return [Dict(names, x) for x in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
+
+
+# @with_connection
+def select_one(sql, *args):
+    return _select(sql, True, *args)
+
+
+# @with_connection
+def select_int(sql, *args):
+    d = _select(sql, True, *args)
+    # 若只返回一条记录，则为DICT，此时仍为一条记录
+    if len(d) != 1 and not isinstance(d, Dict):
+        raise MultiColumnsError('Expect only one column.')
+    return d.values()[0]
+
+
+# @with_connection
+def select(sql, *args):
+    return _select(sql, False, *args)
 
 
 @with_connection
+def _update(sql, *args):
+    """
+    update实现函数
+    :param sql:
+    :param args:
+    :return:
+    """
+    global _db_ctx
+    sql = sql.replace('?', '%s')
+    cursor = None
+    logging.info("Sql: %s, Args: %s" % (sql, args))
+    try:
+        cursor = _db_ctx.cursor()
+        cursor.execute(sql, args)
+        r = cursor.rowcount
+        # 当不处于事务状态下，需要提交
+        if _db_ctx.transactions == 0:
+            _db_ctx.connection.commit()
+            logging.info("auto commit")
+        return r
+    finally:
+        if cursor:
+            cursor.close()
+
+
 def update(sql, *args):
-    pass
+    return _update(sql, *args)
+
+
+def insert(table, **kw):
+    """
+    Execute insert SQL.
+    :param table: 表明
+    :param kw: sql参数
+    :return: 影响行数
+
+    >>> update("delete from user where id = 2000")
+    1
+    >>> u1 = dict(id=2000, name='Bob', email='bob@test.org', passwd='bobobob', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> u2 = select_one('select * from user where id=?', 2000)
+    >>> u2.name
+    u'Bob'
+    >>> insert('user', **u2)
+    Traceback (most recent call last):
+      ...
+    IntegrityError: 1062 (23000): Duplicate entry '2000' for key 'PRIMARY'
+    """
+    # 加*是zip的参数，把迭代器转化为list
+    cols, args = zip(*kw.iteritems())
+    sql = 'insert into %s (%s) values (%s)' % \
+          (table, ','.join(['%s' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
+
 
 class _TransactionCtx(object):
     def __enter__(self):
@@ -106,34 +386,75 @@ class _TransactionCtx(object):
         if not _db_ctx.is_init():
             _db_ctx.init()
             self.should_close_conn = True
-        _db_ctx.transactions = _db_ctx.transactions + 1
+        _db_ctx.transactions += 1
+        logging.info('begin transaction...' if _db_ctx.transactions == 1 else 'join current transaction...')
         return self
 
-    def __exit__(self, exctype, excvalue, traceback):
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        exception_logging(exc_type, exc_value, exc_tb, "_TransactionCtx.__exit__")
         global _db_ctx
-        _db_ctx.transactions = _db_ctx.transactions - 1
+        # _db_ctx.transactions = _db_ctx.transactions - 1
+        _db_ctx.transactions -= 1
         try:
-            if _db_ctx.transactions==0:
-                if exctype is None:
+            if _db_ctx.transactions == 0:
+                if exc_type is None:
                     self.commit()
                 else:
                     self.rollback()
         finally:
             if self.should_close_conn:
                 _db_ctx.cleanup()
+            logging.info('end transaction...')
 
-    def commit(self):
+    @staticmethod
+    def commit():
         global _db_ctx
+        logging.info('commit transaction...')
         try:
             _db_ctx.connection.commit()
+            logging.info('commit ok.')
         except:
+            logging.warning('commit failed. try rollback...')
             _db_ctx.connection.rollback()
+            logging.warning('rollback ok.')
             raise
 
-    def rollback(self):
+    @staticmethod
+    def rollback():
         global _db_ctx
+        logging.warning('rollback transaction...')
         _db_ctx.connection.rollback()
+        logging.info('rollback ok.')
 
 
+def transaction():
+    return _TransactionCtx()
+
+
+def with_transaction(func):
+    start = time.time()
+
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        with transaction():
+            func(*args, **kw)
+        _profiling(start)
+    return wrapper
+
+
+# curs = _LasyConnection().cursor()的写法会导致弱连接（连接可能被垃圾回收），导致报错；应写为2句话
 if __name__ == "__main__":
-    print select("select * from user where id = ?", '1')
+    create_engine('root', 'password', 'test')
+
+    # print update('insert into user (id, name) values (?, ?)', '4', 'John')
+    # print select("select * from user")
+
+    #update('drop table if exists user')
+    #update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
+
+    #update("delete from user where id =?", 1)
+    #u1 = dict(id=1, name='Bob', email='bob@test.org', passwd='bobobob', last_modified=time.time())
+    #insert('user', **u1)
+    #print select("select * from user")
+    import doctest
+    doctest.testmod(verbose=True)
